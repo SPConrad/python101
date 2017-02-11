07444087a603d7c0e40a6642f816b2b4602575cc
Sean Conrad <konrad9@gmail.com>
2017-02-11 17:48:58 -0500

    code now conforms to python standards

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,41 +2,39 @@ import media
 import fresh_tomatoes
 
 # This file contains prepared sample movies for the movie array,
-# populates the array, and opens the movie page. 
+# populates the array, and opens the movie page.
 
-toy_story = media.Movie("Toy Story", "Toys do stuff", "https://upload.wikimed"\
-                        + "ia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px"\
-                        + "-Toy_Story.jpg", "https://www.youtube.com/watch?v="\
-                        + "KYz2wyBy3kc")
+toy_story = media.Movie("Toy Story", "Toys do stuff", "https://upload.wikimed" +
+                        "ia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px" +
+                        "-Toy_Story.jpg", "https://www.youtube.com/watch?v=" +
+                        "KYz2wyBy3kc")
 
-avatar = media.Movie("Avatar", "Aliens and stuff", "https://upload.wikimedia."\
-                     + "org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/"\
-                     + "220px-Avatar-Teaser-Poster.jpg","https://www.youtube."\
+avatar = media.Movie("Avatar", "Aliens and stuff", "https://upload.wikimedia."
+                     + "org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/"
+                     + "220px-Avatar-Teaser-Poster.jpg", "https://www.youtube."
                      + "com/watch?v=cRdxXPV9GNQ")
 
-serenity = media.Movie("Serenity", "They aim to misbehave", "https://upload.w"\
-                       + "ikimedia.org/wikipedia/en/thumb/9/9e/Serenity_One_S"\
-                       + "heet.jpg/215px-Serenity_One_Sheet.jpg", "https://ww"\
+serenity = media.Movie("Serenity", "They aim to misbehave", "https://upload.w"
+                       + "ikimedia.org/wikipedia/en/thumb/9/9e/Serenity_One_S"
+                       + "heet.jpg/215px-Serenity_One_Sheet.jpg", "https://ww"
                        + "w.youtube.com/watch?v=6nEAlpTb4tk")
 
-avengers = media.Movie("Avengers", "They assemble", "https://upload.wikimedia"\
-                       + ".org/wikipedia/en/thumb/f/f9/TheAvengers2012Poster."\
-                       + "jpg/220px-TheAvengers2012Poster.jpg", "https://www."\
+avengers = media.Movie("Avengers", "They assemble", "https://upload.wikimedia"
+                       + ".org/wikipedia/en/thumb/f/f9/TheAvengers2012Poster."
+                       + "jpg/220px-TheAvengers2012Poster.jpg", "https://www."
                        + "youtube.com/watch?v=eOrNdBpGMv8")
 
-skyfall = media.Movie("Skyfall", "Bond wins, but also loses", "https://upload"\
-                      + ".wikimedia.org/wikipedia/en/thumb/a/a7/Skyfall_poste"\
-                      + "r.jpg/220px-Skyfall_poster.jpg", "https://www.youtub"\
+skyfall = media.Movie("Skyfall", "Bond wins, but also loses", "https://upload"
+                      + ".wikimedia.org/wikipedia/en/thumb/a/a7/Skyfall_poste"
+                      + "r.jpg/220px-Skyfall_poster.jpg", "https://www.youtub"
                       + "e.com/watch?v=6kw1UVovByw")
 
-the_martian = media.Movie("The Martian", "Matt Damon gets stranded AGAIN", "h"\
-                          + "ttps://upload.wikimedia.org/wikipedia/en/thumb/c"\
-                          + "/cd/The_Martian_film_poster.jpg/220px-The_Martia"\
-                          + "n_film_poster.jpg", "https://www.youtube.com/wat"\
+the_martian = media.Movie("The Martian", "Matt Damon gets stranded AGAIN", "h"
+                          + "ttps://upload.wikimedia.org/wikipedia/en/thumb/c"
+                          + "/cd/The_Martian_film_poster.jpg/220px-The_Martia"
+                          + "n_film_poster.jpg", "https://www.youtube.com/wat"
                           + "ch?v=ej3ioOneTy8")
-
 
 movies = [toy_story, avatar, serenity, avengers, skyfall, the_martian]
 
 fresh_tomatoes.open_movies_page(movies)
-
--- media.py
+++ media.py
@@ -1,13 +1,15 @@
 import webbrowser
 
-#this file contains the Movie class defintion
+
+# this file contains the Movie class defintion
+
 
 class Movie():
-    """ This class provides a way to store movie related information"""
-    
+    # This class provides a way to store movie related information
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -15,5 +17,3 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
-        
